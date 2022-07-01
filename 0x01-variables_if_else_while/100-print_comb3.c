c72965c456fc2b224a22e8ac5d787322f05d4ded
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-01 20:43:55 +0000

    The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,23 +17,24 @@ int main(void)
 	{
 		while (j <= '9')
 		{
-			if (!(i < j || i == j))
+			putchar(i);
+			putchar(j);
+			if (i == '9' && j == '9')
 			{
-				putchar(i);
-				putchar(j);
-				if (i == '8' && j =='9')
-				{
-					putchar('\n');
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
 			}
 			j++;
 		}
-		j = '0';
+		if (j >= '9')
+		{
+			
+			j = '0';
+		}
 		i++;
 	}
 	return (0);
