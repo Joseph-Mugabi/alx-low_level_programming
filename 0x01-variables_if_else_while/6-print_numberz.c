7b9fb382f57957ed88cd2e624872553acf0c2eb5
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-01 19:15:07 +0000

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,17 +4,19 @@
 
 /**
  * main - prints all single digit numbers of base 10 starting from 0.
- * All your code should be in the main function
+ * followed by a new line.
  * Return: Always 0
  */
 int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
+	for (n =0; n < 10; n++)
 	{
 		printf("%d", n);
 	}
 	printf("\n");
 	return (0);
 }
+
+
