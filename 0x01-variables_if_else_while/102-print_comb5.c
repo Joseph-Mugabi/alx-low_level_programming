1c8aee7f199da20088f1957c9fb369f7d9c43ccf
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-01 22:03:10 +0000

    software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers.
+ * The numbers should range from 0 to 99
+ * The two numbers should be separated by a space
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = 0; j <= 99; j++)
+		{
+			if (i < j && i != j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
