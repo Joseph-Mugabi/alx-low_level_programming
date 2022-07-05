ac401beface1ac3aa502e70facd4407601e0980d
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-05 23:03:52 +0000

    Learm your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,6 +19,7 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			c = (a * b);
+
 			if ((c / 10) > 0)
 			{
 				_putchar((c / 10) + '0');
@@ -28,6 +29,7 @@ void times_table(void)
 				_putchar(' ');
 			}
 			_putchar((c % 10) + '0');
+
 			if (b < 9)
 			{
 				_putchar(',');
