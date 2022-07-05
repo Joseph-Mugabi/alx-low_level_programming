1b1ad2777ad614f1e04fe5d5c68f9e4fa7fc31d5
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-05 23:17:26 +0000

    addition of a + a

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - function that adds two integers and returns the result.
+ * @x: int x
+ * @y: int y
+ * Return: 0
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
