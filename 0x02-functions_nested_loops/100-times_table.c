63ab3917952da9341108c3d1c3faf3472759cf9b
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-05 23:56:55 +0000

    mathematical equation

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_times_table - function that prints the n times table, starting with 0.
+ * @n: input
+ * Return: 0
+ */
+void print_times_table(int n)
+{
+	return (0);
+}
