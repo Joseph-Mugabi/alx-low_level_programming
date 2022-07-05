6b3bb5c556428d48ffa13c18c6d64e0791a7f302
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-05 22:50:00 +0000

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,13 +21,13 @@ void times_table(void)
 			c = (a * b);
 			if ((c / 10) > 0)
 			{
-				_putchar((d / 10) + '0');
+				_putchar((c / 10) + '0');
 			}
 			else
 			{
 				_putchar(' ');
 			}
-			_putchar((d % 10) + '0');
+			_putchar((c % 10) + '0');
 			if (b < 9)
 			{
 				_putchar(',');
