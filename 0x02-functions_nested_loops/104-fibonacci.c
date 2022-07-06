7c8a9d71f1a2e1747ddc7711495360d9d1d54517
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-06 09:38:46 +0000

    print the first 100 Fibonacci numbers.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,43 +6,29 @@
  */
 int main(void)
 {
-	int inc;
+	long n1 = 1;
+	long n2 = 1;
 
-	unsigned long n1 = 0, n2 = 1, n3;
-	unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
-	unsigned long h1, h2;
+	int i;
+	long sum;
+	long aux = 0;
 
-	for (inc = 0; inc < 92; inc++)
+	for (i = 0; i < 50; i++)
 	{
-		n3 = n1 + n2;
-		printf("%lu, ", n3);
-		n1 = n2;
-		n2 = n3;
+		if (!(i == 0))
 		{
-			n1_h1 = n1 / 10000000000;
-			n2_h1 = n2 / 10000000000;
-			n1_h2 = n1 % 10000000000;
-			n2_h2 = n2 % 10000000000;
-			for (inc = 93; inc < 99; inc++)
+			sum = n1 + n2;
+			n1 = n2;
+			n2 = sum;
+			if (sum < 4000000)
 			{
-				h1 = n1_h1 + n2_h1;
-				h2 = n1_h2 + n2_h2;
-				if ((n1_h2 + n2_h2) > 9999999999)
+				if ((sum % 2) == 0)
 				{
-					h1 += 1;
-					h2 %= 1000000000;
+					aux = aux + sum;
 				}
-				printf("%lu%lu", h1, h2);
-				if (inc != 98)
-					printf(",");
-					n1_h1 = n2_h1;
-					n1_h2 = n2_h2;
-					n2_h1 = h1;
-					n2_h2 = h2;
-			}	
+			}
 		}
-	}
+	}printf("%ld", aux);
 	printf("\n");
 	return (0);
 }
-
