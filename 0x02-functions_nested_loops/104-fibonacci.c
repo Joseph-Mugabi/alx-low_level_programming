48a6e40cc0d10a20caf1ed380c5c7105778a62ef
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-06 09:50:10 +0000

    program to print the first 100 Fibonacci numbers.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,29 +6,43 @@
  */
 int main(void)
 {
-	long n1 = 1;
-	long n2 = 1;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long half1, half2;
 
-	int i;
-	long sum;
-	long aux = 0;
-
-	for (i = 0; i < 50; i++)
+	for (count = 0; count < 92; count++)
 	{
-		if (!(i == 0))
+		sum = fib1 + fib2;
+		printf("%lu, ", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+	}
+
+	fib1_half1 = fib1 / 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
+	fib2_half2 = fib2 % 10000000000;
+
+	for (count = 93; count < 99; count++)
+	{
+		half1 = fib1_half1 + fib2_half1;
+		half2 = fib1_half2 + fib2_half2;
+		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
-			sum = n1 + n2;
-			n1 = n2;
-			n2 = sum;
-			if (sum < 4000000)
-			{
-				if ((sum % 2) == 0)
-				{
-					aux = aux + sum;
-				}
-			}
+			half1 += 1;
+			half2 %= 10000000000;
 		}
-	}printf("%ld", aux);
+		printf("%lu%lu", half1, half2);
+		if (count != 98)
+			printf(", ");
+		fib1_half1 = fib2_half1;
+		fib1_half2 = fib2_half2;
+		fib2_half1 = half1;
+		fib2_half2 = half2;
+	}
 	printf("\n");
 	return (0);
 }
+
