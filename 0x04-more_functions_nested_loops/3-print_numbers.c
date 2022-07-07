ad23d3ceb0102e764266490ba61e50516c745a45
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-07 22:51:32 +0000

    numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,12 @@
 
 /**
  * print_numbers - function that prints the numbers,
- * 0 to 9
- * Return: void
  */
 void print_numbers(void)
 {
 	int a = 0;
 
 	for (a = 0; a < 10; a++)
-	{
-		_putchar(a);
-		a++;
-	}
-
+		_putchar(a + '0');
 	_putchar('\n');
 }
