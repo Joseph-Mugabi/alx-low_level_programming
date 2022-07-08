eb751cfeabee5bfbfee7fb7114db83bb080bec0d
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-08 00:06:36 +0000

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,24 +12,24 @@ int main(void)
 
 	while (num++ < 100)
 
-	if ((num % 3 == 0) && (num % 5 == 0))
-		printf("FizzBuzz ");
+		if ((num % 3 == 0) && (num % 5 == 0))
+			printf("FizzBuzz ");
 
-	else if ((num % 3) == 0)
-		printf("Fizz ");
+		else if ((num % 3) == 0)
+			printf("Fizz ");
 
-	else if ((num % 5) == 0)
-	{
-		if (num != 100)
-			printf("Buzz ");
+		else if ((num % 5) == 0)
+		{
+			if (num != 100)
+				printf("Buzz ");
+
+			else
+				printf("Buzz");
+		}
 
 		else
-			printf("Buzz");
-	}
+			printf("%d ", num);
 
-	else
-		printf("%d ", num);
-
-		printf("\n");
+	printf("\n");
 	return (0);
 }
