ea25f8779c18842c4581119d8ece82a75ff6855a
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-11 18:15:23 +0000

    defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  _strlen - function that returns the length of a string
+ *  @s: string pointer
+ *  Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	while (*s != '\0')
+	{
+		len += 1;
+		*s = *s + 1;
+	}
+	return (len);
+}
