e20c4e1b23e63f55a244b38759a8ed9e4984328b
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-11 17:45:03 +0000

    swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int -  function that swaps the values of two integers
+ * @a: parametr1
+ * @b: parametr2
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int n;
+
+	n = *a;
+	*a = *b;
+	*b = n;
+}
