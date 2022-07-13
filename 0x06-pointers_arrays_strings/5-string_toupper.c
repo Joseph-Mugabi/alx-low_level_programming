c33a4f685a9d2dcf9428c0880d9ac4fe7292a23a
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-13 18:11:28 +0000

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,12 +7,12 @@
 */
 char *string_toupper(char *s)
 {
-	int i = 0;
+int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-			s[i] = s[i] - 'a' + 'A';
-	}
-	return (s);
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+s[i] = s[i] - 'a' + 'A';
+}
+return (s);
 }
