b072ad1e5ad24b6f361fd7408cbb8079cbbb8e1f
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-14 08:27:45 +0000

    more effective than cubicles or booth walls

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+/*
+* print_buffer - function that prints a buffer.
+* @b: buffer
+* @size: buffer to point
+* Return: void
+*/
+void print_buffer(char *b, int size)
+{
+	int o, p, q;
+
+	o = 0;
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+		p = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (q = 0; q < 10; q++)
+		{
+			if (q < p)
+				printf("%02x", *(b + o + q));
+			else
+				printf(" ");
+			if (q % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (q = 0; q < p; q++)
+		{
+			int c = *(b + o + q);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
+}
