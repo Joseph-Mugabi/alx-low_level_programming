202afef248d1279e9aff81e4c9a5f23750b12564
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-14 11:12:41 +0000

    Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,9 @@
-#include "main.H"
+#include "main.h"
 
 /**
 * cap_string - function that capitalizes all words of a string
 * @s: pointer to char params
-* Return: *s
+* Return: s
 */
 char *cap_string(char *s)
 {
@@ -11,17 +11,17 @@ char *cap_string(char *s)
 
 	if (s[i] >= 'a' && s[i] <= 'z')
 		s[i] = s[i] - 'a' + 'A';
-		i++;
+	i++;
 	while (s[i] != '\0')
 	{
-		if ((s[i] >= 'a' && s[i] <= 'z')
-			&& (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
-			s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
-			s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
-			s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
-			|| s[i - 1] == '\n'))
+		if ((s[i] >= 'a' && s[i] <= 'z') && (s[i - 1] == ',' ||
+			s[i - 1] == ';' || s[i - 1] == '.' || s[i - 1]
+			== '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+			s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1]
+			== '{' || s[i - 1] == '}' || s[i - 1] == ' ' ||
+			s[i - 1] == '\t' || s[i - 1] == '\n'))
 			s[i] = s[i] - 'a' + 'A';
-			i++;
+	i++;
 	}
 	return (s);
 }
