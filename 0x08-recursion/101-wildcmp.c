270ecb5d1bd50cfd23756b62dfcdfbd7e6d8888c
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-21 17:56:16 +0000

    Inception

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+* substring_match - chech if a substring after wildcard matches s1.
+* @s1: first string1
+* @s2: second string
+* @after_wild: placeholder for postion right after wildcard
+* Return: 1 if matched, 0 if not.
+*/
+int substring_match(char *s1, char *s2, char *after_wild)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '*')
+	       return (substring_match(s1, s2 + 1, s2 + 1));
+	if (*s1 == '\0' && *s2 != '0')
+		return (0);
+	if (*s2 == '*')
+		return (substring_match(s1, s2 + 1, s2 + 1));
+	if (*s1 == *s2)
+		return (substring_match(s1 + 1, s2 + 1, after_wild));
+	else
+		return (substring_match(s1 + 1, after_wild, after_wild));
+}
+/**
+* wildcmp - compare if string with wildcard matches.
+* @s1: first string1
+* @s2: second string2
+*
+* Return: 1 if matched, 0 if not.
+*/
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (0);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s2 == '*')
+		return (substring_match(s1, s2 + 1, s2 + 1));
+	else
+		return (0);
+}
+int main(void)
+{
+	int r;
+
+	r = wildcmp("main.c", "*.c");
+	printf("%d\n", r);
+	return (0);
+}
