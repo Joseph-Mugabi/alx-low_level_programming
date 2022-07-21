70ebe3a44840843c873be1df833ac9dcd13baee2
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-07-21 18:07:02 +0000

    Inception

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,7 +12,7 @@ int substring_match(char *s1, char *s2, char *after_wild)
 	if (*s1 == '\0' && *s2 == '\0')
 		return (0);
 	if (*s1 == '\0' && *s2 == '*')
-	       return (substring_match(s1, s2 + 1, s2 + 1));
+		return (substring_match(s1, s2 + 1, s2 + 1));
 	if (*s1 == '\0' && *s2 != '0')
 		return (0);
 	if (*s2 == '*')
