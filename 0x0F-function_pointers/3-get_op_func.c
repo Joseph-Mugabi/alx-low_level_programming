074edb4947c0659dc6fd9b8159da480322f14be9
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-08-03 19:18:53 +0000

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - function to select correct operation func.
+ * @s: operation gvn (+,/,*,%,-)
+ *
+ * Return: pointer to correct op func.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int j = 0;
+
+	while (ops[j].op != NULL)
+	{
+		if (*s == *ops[j].op)
+			return (ops[j].f);
+		j++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+/**
+ * op_add - adds
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substract or difference
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - products of 2 integers
+ * @a: integer
+ * @b: integer
+ * Return: result
+ *
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division of 2 integers
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - reminder of the division of a and b
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
