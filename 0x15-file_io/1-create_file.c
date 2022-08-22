f5e9886d1c99eeb2631fb984a1eabf4b3f073811
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-08-22 18:11:35 +0000

    under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strlen - length of the strng
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s);
+
+int _strlen(char *s)
+{
+	int l = 0; /* length */
+
+	while (*s++)
+		l++;
+	return (l);
+}
+
+/**
+ * create_file - creates a file.
+ * @filename: name of the file to create
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int fd_write;
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_RDWR, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	fd_write = write(fd, text_content, _strlen(text_content));
+	if (fd_write == -1 || fd_write != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+
+	return (1);
+}
