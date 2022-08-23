053d888124c266083dd35fa89f250f61f68eef3e
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-08-23 15:42:57 +0000

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,32 +28,32 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fdo1 = open(argv[1], O_RDONLY);
 	if (fdo1 == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fdo2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
 	if (fdo2 == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
 		exit(99);
 	}
 	do {
 		fdr = read(fdo1, arr, 1024);
 		if (fdr == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 		fdw = write(fdo2, arr, fdr);
 		if (fdw != fdr || fdr == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
 			exit(99);
 		}
 	} while (fdr == 1024);
