0815f727ec1127ae701351a6ee0a3a2d1cb8a39f
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-10-24 14:18:48 +0000

    ordered ht

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -127,7 +127,7 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 		tmp = tmp->next;
 	}
 	shnode = create_shash_node(key, value);
-	if(!shnode)
+	if (!shnode)
 		return (0);
 	shnode->next = ht->array[idx];
 	ht->array[idx] = shnode;
