04a46af5a3200a93d61db5f552c0dacbee0573fd
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-12-08 09:09:43 +0000

    interview

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
 """
-Module to define a fun Island_perimeter
+Module Island Perimeter
 """
 
 
 def island_perimeter(grid):
-    """
-    fun to calculate an island described in a grid parimeter
-    
-    grid is a list of list of integers:
-    0 represents a water zone
-    1 represents a land zone
-    One cell is a square with side length 1
-    Grid cells are connected horizontally/vertically (not diagonally).
-    Grid is rectangular, width and height don’t exceed 100
-    """
-
-    width = len(grid[0])
-    height = len(grid)
-    edges = 0
-    size = 0
-
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j] == 1:
-                size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
-    return size * 4 - edges * 2
-
+    """ Calculate perimeter of grid where "1" is found"""
+    p = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    p += 1  # top
+                if row == (len(grid) - 1) or grid[row + 1][col] == 0:
+                    p += 1  # bottom
+                if col == 0 or grid[row][col - 1] == 0:
+                    p += 1  # left
+                if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
+                    p += 1  # right
+    return p
