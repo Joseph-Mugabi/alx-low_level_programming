cf531f274a7a0a2bcd66d990c69acba73be1a672
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-12-08 08:51:21 +0000

    makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module to define a fun Island_perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    fun to calculate an island described in a grid parimeter
+    
+    grid is a list of list of integers:
+    0 represents a water zone
+    1 represents a land zone
+    One cell is a square with side length 1
+    Grid cells are connected horizontally/vertically (not diagonally).
+    Grid is rectangular, width and height don’t exceed 100
+    """
+
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
+
